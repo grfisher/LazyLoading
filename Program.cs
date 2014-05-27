750ffeef55f0a3f1dc598c716e2764824676ceb0
grfisher <gregor_fisher@yahoo.com>
2014-05-27 15:26:05 -0700

    newest change

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace LazyLoading
         private static Customer customer = null;
         static void Main(string[] args)
         {
-            Customer o = new Customer(); // order object not loadedxzzz
+            Customer o = new Customer(); // order object not loadedxzzzffxx
             Console.WriteLine(o.CustomerName);
             
             foreach (Order o1 in o.Orders) // Load order object only at this moment
